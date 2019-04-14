5afe767b85ccc3a8975bee9d0fa2b09472fad5ec
Tim <sarrtim@gmail.com>
2019-04-14 14:15:37 +0300

    Add api import

--- app/api_imports.py
+++ app/api_imports.py
@@ -1,3 +1,3 @@
 from app.core import root_api
-from app.account.api import generate_jwt_api, refresh_jwt_api, user_api, online_status_api
+from app.account.api import generate_jwt_api, refresh_jwt_api, user_api, online_status_api, total_quizzes_api
 from app.quiz.api import quiz_api
