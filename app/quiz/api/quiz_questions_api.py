9eb6ba9de80c8d5861631963d085324748a60452
Tim <sarrtim@gmail.com>
2019-04-14 19:32:08 +0300

    Add quiz_questions_api

---
+++ app/quiz/api/quiz_questions_api.py
@@ -0,0 +1,24 @@
+from flask import jsonify
+from flask.views import MethodView
+from flask_cors import cross_origin
+from flask_jwt_extended import jwt_required
+from app import app
+from app.quiz.models import Quiz
+
+
+class QuizQuestionsApi(MethodView):
+    @cross_origin()
+    @jwt_required
+    def get(self, pk):
+        quiz = Quiz.get_by_id(pk)
+        questions = quiz.questions
+        response_dict = dict(count=len(questions))
+        results = []
+        for v in questions:
+            question_dict = v.__dict__
+            del question_dict['_sa_instance_state']
+            results.append(question_dict)
+        return jsonify(response_dict)
+
+app.add_url_rule('/quiz/<pk>/questions', view_func=QuizQuestionsApi.as_view('quiz-questions'),
+                 methods=['GET', 'POST', 'PUT', 'DELETE'])
