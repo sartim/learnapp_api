30fcdc547deea1f5aec8ff150fa348a59a947263
Tim <sarrtim@gmail.com>
2019-04-15 00:43:22 +0300

    Add answer field to model

--- app/quiz/question/models.py
+++ app/quiz/question/models.py
@@ -1,8 +1,11 @@
+import os
+
 from app import db
 from app.core.models import Base
 from app.quiz.models import Quiz
 from app.quiz.question.type.models import QuizQuestionType
 from app.quiz.section.models import QuizSection
+from app.helpers import utils
 
 
 class QuizQuestion(Base):
@@ -14,14 +17,37 @@ class QuizQuestion(Base):
     question_type_id = db.Column(db.Integer, db.ForeignKey('quiz_question_types.id'))
     choices = db.Column(db.JSON, nullable=True)
     section_id = db.Column(db.Integer, db.ForeignKey('quiz_sections.id'))
+    answer = db.Column(db.String(255))
 
     quiz = db.relationship(Quiz, backref='quiz', lazy=True)
     type = db.relationship(QuizQuestionType, backref='quiz_question_type', lazy=True)
     section = db.relationship(QuizSection, backref='quiz_section', lazy=True)
 
-    def __init__(self, quiz_id=None, question=None, question_type_id=None, choices=None, section_id=None):
+    def __init__(self, quiz_id=None, question=None, question_type_id=None, choices=None, section_id=None, answer=None):
         self.quiz_id = quiz_id
         self.question = question
         self.question_type_id = question_type_id
         self.choices = choices
         self.section_id = section_id
+        self.answer = answer
+
+    @classmethod
+    def response(cls, paginated_objs, url):
+        results = []
+        for obj in paginated_objs.items:
+            data = dict(
+                quiz_id=obj.quiz_id, question=obj.question, question_type_id=obj.question_type_id, choices=obj.choices,
+                section_id=obj.section_id
+            )
+            results.append(data)
+        data = utils.response_dict(paginated_objs, results, url)
+        return data
+
+    @classmethod
+    def get_all(cls, page):
+        """
+        Retrieve all quizzes regardless of creator
+        :return:
+        """
+        paginated_objs = cls.query.paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')), error_out=False)
+        return cls.response(paginated_objs, '/quiz/')
