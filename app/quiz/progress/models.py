852d980ba508cff7841e6bcd31837418c69f8f5f
Tim <sarrtim@gmail.com>
2019-04-16 20:30:18 +0300

    Add QuizProgress Model

---
+++ app/quiz/progress/models.py
@@ -0,0 +1,38 @@
+import os
+
+from flask_jwt_extended import current_user
+from sqlalchemy import desc
+from app.account.user.authenticated.models import AccountUserAuthenticated
+from app.account.user.models import AccountUser
+from app.account.user.role.models import AccountUserRole
+from app.core.mixins import SearchableMixin
+from app.core.models import Base
+from app import db
+from app.quiz.models import Quiz
+from app.quiz.status.models import QuizStatus
+
+
+class QuizProgress(Base):
+
+    __tablename__ = 'quiz_progress'
+
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quizzes.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('account_users'))
+    status_id = db.Column(db.Integer, db.ForeignKey('quiz_statuses'))
+    started_date_time = db.Column(db.DateTime)
+    ended_date_time = db.Column(db.DateTime)
+
+    quiz = db.relationship(Quiz, backref='quiz', lazy=True)
+    user = db.relationship(AccountUser, backref='account_user', lazy=True)
+    status = db.relationship(QuizStatus, backref='quiz_status', lazy=True)
+
+
+    def __init__(self, quiz_id=None, user_id=None, status_id=None, started_date_time=None, ended_date_time=None):
+        self.quiz_id = quiz_id
+        self.user_id = user_id
+        self.status_id = status_id
+        self.started_date_time = started_date_time
+        self.ended_date_time = ended_date_time
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, self.id)
