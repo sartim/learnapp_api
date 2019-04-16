26e4a6ad02e0c1ae2800d83cdb82649b977a7b7d
Tim <sarrtim@gmail.com>
2019-04-16 19:56:41 +0300

    Add responses model

---
+++ app/quiz/question/responses/models.py
@@ -0,0 +1,21 @@
+from app import db
+from app.account.user.models import AccountUser
+from app.core.models import Base
+from app.quiz.question.models import QuizQuestion
+
+
+class QuizQuestionResponse(Base):
+
+    __tablename__ = 'quiz_questions'
+
+    question_id = db.Column(db.Integer, db.ForeignKey('quiz_questions.id'))
+    response = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('account_users.id'))
+
+    question = db.relationship(QuizQuestion, backref='quiz', lazy=True)
+    user = db.relationship(AccountUser, backref='account_user', lazy=True)
+
+    def __init__(self, question_id=None, response=None, user_id=None):
+        self.question_id = question_id
+        self.response = response
+        self.user_id = user_id
