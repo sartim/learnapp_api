bdb041daa35a4f8f7e30e2398cce3cffec69f929
Tim <sarrtim@gmail.com>
2019-04-16 16:53:00 +0300

    Wrap to function

--- app/core/connection.py
+++ app/core/connection.py
@@ -2,25 +2,28 @@ import os, re, logging
 from elasticsearch import Elasticsearch
 import certifi
 
-# Log transport details (optional):
-logging.basicConfig(level=logging.INFO)
 
-# Parse the auth and host from env:
-bonsai = os.environ.get('ES_URL')
-auth = re.search('https\:\/\/(.*)\@', bonsai).group(1).split(':')
-host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
+def elasticsearch():
+    # Log transport details (optional):
+    logging.basicConfig(level=logging.INFO)
 
-# Connect to cluster over SSL using auth for best security:
-es_header = [{
- 'host': host,
- 'port': 443,
- 'use_ssl': True,
- 'http_auth': (auth[0],auth[1]),
- 'ca_certs': certifi.where()
-}]
+    # Parse the auth and host from env:
+    bonsai = os.environ.get('ES_URL')
+    auth = re.search('https\:\/\/(.*)\@', bonsai).group(1).split(':')
+    host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
 
-# Instantiate the new Elasticsearch connection:
-elastic_search = Elasticsearch(es_header)
+    # Connect to cluster over SSL using auth for best security:
+    es_header = [{
+     'host': host,
+     'port': 443,
+     'use_ssl': True,
+     'http_auth': (auth[0],auth[1]),
+     'ca_certs': certifi.where()
+    }]
 
-# Verify that Python can talk to Bonsai (optional):
-elastic_search.ping()
+    # Instantiate the new Elasticsearch connection:
+    elastic_search = Elasticsearch(es_header)
+
+    # Verify that Python can talk to Bonsai (optional):
+    elastic_search.ping()
+    return elasticsearch
