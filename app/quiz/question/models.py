7894ed167da814f084c10f93ae8a0e9680e03db1
Tim <sarrtim@gmail.com>
2019-04-17 21:18:53 +0300

    Refactor QuizQuestion model

--- app/quiz/question/models.py
+++ app/quiz/question/models.py
@@ -4,7 +4,6 @@ from app import db
 from app.core.models import Base
 from app.quiz.models import Quiz
 from app.quiz.question.type.models import QuizQuestionType
-from app.quiz.section.models import QuizSection
 from app.helpers import utils
 
 
@@ -15,21 +14,13 @@ class QuizQuestion(Base):
     quiz_id = db.Column(db.Integer, db.ForeignKey('quizzes.id'))
     question = db.Column(db.String(255))
     question_type_id = db.Column(db.Integer, db.ForeignKey('quiz_question_types.id'))
-    choices = db.Column(db.JSON, nullable=True)
-    section_id = db.Column(db.Integer, db.ForeignKey('quiz_sections.id'))
-    answer = db.Column(db.String(255))
-
     quiz = db.relationship(Quiz, backref='quiz', lazy=True)
     type = db.relationship(QuizQuestionType, backref='quiz_question_type', lazy=True)
-    section = db.relationship(QuizSection, backref='quiz_section', lazy=True)
 
-    def __init__(self, quiz_id=None, question=None, question_type_id=None, choices=None, section_id=None, answer=None):
+    def __init__(self, quiz_id=None, question=None, question_type_id=None):
         self.quiz_id = quiz_id
         self.question = question
         self.question_type_id = question_type_id
-        self.choices = choices
-        self.section_id = section_id
-        self.answer = answer
 
     @classmethod
     def response(cls, paginated_objs, url):
