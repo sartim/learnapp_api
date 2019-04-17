9c57e55c6e5c04919c33da7b11ad2627a13a4ee6
Tim <sarrtim@gmail.com>
2019-04-17 21:20:35 +0300

    Add section field to Quiz model

--- app/quiz/models.py
+++ app/quiz/models.py
@@ -5,6 +5,7 @@ from app import db
 from app.core.mixins import SearchableMixin
 from app.core.models import Base
 from app.helpers import utils
+from app.quiz.section.models import QuizSection
 
 
 class Quiz(Base, SearchableMixin):
@@ -18,18 +19,21 @@ class Quiz(Base, SearchableMixin):
     video_url = db.Column(db.Text, nullable=True)
     time_to_take = db.Column(db.Integer, nullable=True)
     needs_invite = db.Column(db.Boolean, default=False)
+    section_id = db.Column(db.Integer, db.ForeignKey('quiz_sections.id'))
 
     creator = db.relationship('AccountUser', backref='creator', lazy=True)
     questions = db.relationship('QuizQuestion', backref='quiz_questions', lazy=True)
+    section = db.relationship(QuizSection, backref='quiz_section', lazy=True)
 
     def __init__(self, name=None, description=None, creator_id=None, video_url=None, time_to_take=None,
-                 needs_invite=None):
+                 needs_invite=None, section_id=None):
         self.name = name
         self.description = description
         self.creator_id = creator_id
         self.video_url = video_url
         self.time_to_take = time_to_take
         self.needs_invite = needs_invite
+        self.section_id = section_id
 
     @classmethod
     def response(cls, paginated_objs, url):
