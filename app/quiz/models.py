aabe724411301b526ea995185ae81bc232cf582b
Tim <sarrtim@gmail.com>
2019-04-18 13:10:49 +0300

    Add rating field

--- app/quiz/models.py
+++ app/quiz/models.py
@@ -20,13 +20,14 @@ class Quiz(Base, SearchableMixin):
     time_to_take = db.Column(db.Integer, nullable=True)
     needs_invite = db.Column(db.Boolean, default=False)
     section_id = db.Column(db.Integer, db.ForeignKey('quiz_sections.id'))
+    rating = db.Column(db.Integer, nullable=True)
 
     creator = db.relationship('AccountUser', backref='creator', lazy=True)
     questions = db.relationship('QuizQuestion', backref='quiz_questions', lazy=True)
     section = db.relationship(QuizSection, backref='quiz_section', lazy=True)
 
     def __init__(self, name=None, description=None, creator_id=None, video_url=None, time_to_take=None,
-                 needs_invite=None, section_id=None):
+                 needs_invite=None, section_id=None, rating=None):
         self.name = name
         self.description = description
         self.creator_id = creator_id
@@ -34,6 +35,7 @@ class Quiz(Base, SearchableMixin):
         self.time_to_take = time_to_take
         self.needs_invite = needs_invite
         self.section_id = section_id
+        self.rating = rating
 
     @classmethod
     def response(cls, paginated_objs, url):
