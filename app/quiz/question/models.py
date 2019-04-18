34116daad7e7957a6059126e4deabc1eb7a7b761
Tim <sarrtim@gmail.com>
2019-04-18 21:34:53 +0300

    Add order_num field

--- app/quiz/question/models.py
+++ app/quiz/question/models.py
@@ -12,6 +12,7 @@ class QuizQuestion(Base):
     __tablename__ = 'quiz_questions'
 
     quiz_id = db.Column(db.Integer, db.ForeignKey('quizzes.id'))
+    order_num = db.column(db.Integer)
     question = db.Column(db.String(255))
     question_type_id = db.Column(db.Integer, db.ForeignKey('quiz_question_types.id'))
     answer = db.Column(db.TEXT)
@@ -19,11 +20,12 @@ class QuizQuestion(Base):
     quiz = db.relationship(Quiz, backref='quiz_questions_quiz', lazy=True)
     type = db.relationship(QuizQuestionType, backref='quiz_question_type', lazy=True)
 
-    def __init__(self, quiz_id=None, question=None, question_type_id=None, answer=None):
+    def __init__(self, quiz_id=None, order_num=None, question=None, question_type_id=None, answer=None):
         self.quiz_id = quiz_id
         self.question = question
         self.question_type_id = question_type_id
         self.answer = answer
+        self.order_num = order_num
 
     @classmethod
     def response(cls, paginated_objs, url):
