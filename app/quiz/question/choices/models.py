df4bbbb33aeb33d20c65ac776a954df94582347e
Tim <sarrtim@gmail.com>
2019-04-18 15:35:09 +0300

    Update model param

--- app/quiz/question/choices/models.py
+++ app/quiz/question/choices/models.py
@@ -1,15 +1,30 @@
-from app import db
+from app import db, app
 from app.quiz.question.models import QuizQuestion
 
 
-class QuizQuestionChoices():
+class QuizQuestionChoice(db.Model):
 
     ___tablename__ ='quiz_question_choices'
 
     question_id = db.Column(db.Integer, db.ForeignKey('quiz_questions.id'), primary_key=True)
     choices = db.Column(db.JSON)
 
-    question = db.relationship(QuizQuestion, backref='quiz', lazy=True)
+    question = db.relationship(QuizQuestion, backref='quiz_question_choice', lazy=True)
 
-    def __init__(self, choices=None):
+    def __init__(self, question_id=None, choices=None):
+        self.question_id = question_id
         self.choices = choices
+
+    @staticmethod
+    def create(obj):
+        db.session.add(obj)
+        obj.save()
+        return obj
+
+    @classmethod
+    def save(cls):
+        try:
+            db.session.commit()
+            app.logger.debug('Successfully committed {} instance'.format(cls.__name__))
+        except Exception:
+            app.logger.exception('Exception occurred. Could not save {} instance.'.format(cls.__name__))
